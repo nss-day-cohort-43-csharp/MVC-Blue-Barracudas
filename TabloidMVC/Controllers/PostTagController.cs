434c6b585b5e2382569d2aa927c91676f8d93f0b
corneliajohnson <corneliaj91@gmail.com>
2021-01-05 11:04:43 -0500

    listed used and unused tags

--- TabloidMVC/Controllers/PostTagController.cs
+++ TabloidMVC/Controllers/PostTagController.cs
@@ -3,16 +3,21 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using TabloidMVC.Repositories;
+using TabloidMVC.Models.ViewModels;
 
 namespace TabloidMVC.Models
 {
     public class PostTagController : Controller
     {
         private readonly IPostTagRepository _postTagRepo;
+        private readonly ITagRepository _tagRepo;
+        private readonly IPostRepository _postRepo;
 
-        public PostTagController(IPostTagRepository postTagRepo)
+        public PostTagController(IPostTagRepository postTagRepo, ITagRepository tagRepo, IPostRepository postRepo)
         {
             _postTagRepo = postTagRepo;
+            _tagRepo = tagRepo;
+            _postRepo = postRepo;
         }
 
         // GET: PostTagsController
@@ -21,9 +26,27 @@ namespace TabloidMVC.Models
             //gey postId fro query string
             int postId = Int32.Parse(HttpContext.Request.Query["postId"]);
 
-            List<PostTag> postTags = _postTagRepo.GetPostTagsbyPostId(postId);
+            Post post = _postRepo.GetPublishedPostById(postId);
 
-            return View(postTags);
+            List<PostTag> postTags = _postTagRepo.GetPostTagsbyPostId(postId);;
+
+            List<Tag> tags = _tagRepo.GetAllTags();
+
+            //add tagids the are attached to post
+            foreach(PostTag pTag in postTags)
+            {
+                int tagId = pTag.TagId;
+                tags.RemoveAll(t => t.Id == tagId);
+            }
+
+            PostTagIndexViewModel vm = new PostTagIndexViewModel
+            {
+                Post = post,
+                PostTags = postTags,
+                Tags = tags
+            };
+
+            return View(vm);
         }
 
         // GET: PostTagsController/Details/5
@@ -75,6 +98,7 @@ namespace TabloidMVC.Models
         }
 
         // GET: PostTagsController/Delete/5
+        //Soft Delete
         public ActionResult Delete(int id)
         {
             return View();
