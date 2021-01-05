c7852f82e23d6c79bb1895a4e127a76123d46472
corneliajohnson <corneliaj91@gmail.com>
2021-01-05 01:33:09 -0500

    added create tag link

--- TabloidMVC/Controllers/TagController.cs
+++ TabloidMVC/Controllers/TagController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TabloidMVC.Models;
 using TabloidMVC.Repositories;
 
 namespace TabloidMVC.Controllers
@@ -39,15 +40,16 @@ namespace TabloidMVC.Controllers
         // POST: TagController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(Tag tag)
         {
             try
             {
+                _tagRepo.AddTag(tag);
                 return RedirectToAction(nameof(Index));
             }
-            catch
+            catch (Exception ex)
             {
-                return View();
+                return View(tag);
             }
         }
 
